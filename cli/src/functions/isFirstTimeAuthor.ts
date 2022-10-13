30f8aa9f21e1665632b500b19ae894e4c3d008a5
Slowlife <slowlife1165@gmail.com>
2022-10-13 05:32:22 +0000

    feat: vscode extension

--- cli/src/functions/isFirstTimeAuthor.ts
+++ cli/src/functions/isFirstTimeAuthor.ts
@@ -1,8 +1,16 @@
 import { readFile } from "fs/promises";
 import { globby } from "globby";
 
+<<<<<<< HEAD
 export default async function isFirstTimeAuthor(author: string) {
   for (const m of await globby("websites/*/*/metadata.json")) {
+=======
+export default async function isFirstTimeAuthor(
+  author: string,
+  custuomPath?: string
+) {
+  for (const m of await globby(custuomPath ?? "websites/*/*/metadata.json")) {
+>>>>>>> b5dbc10 (feat: vscode extension)
     const {
       author: { id },
     } = JSON.parse(await readFile(m, "utf-8"));
