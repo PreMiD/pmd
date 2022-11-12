3da844ead09c252655122340b01b0c756390724d
Slowlife <slowlife1165@gmail.com>
2022-11-12 05:23:05 +0000

    fix merge confilts

--- cli/src/functions/getPresences.ts
+++ cli/src/functions/getPresences.ts
@@ -1,17 +1,10 @@
 import { readFile } from "fs/promises";
 import { globby } from "globby";
 
-<<<<<<< HEAD
 export default async function getPresences() {
   return await Promise.all(
     (
       await globby("websites/*/*/metadata.json")
-=======
-export default async function getPresences(custuomPath?: string) {
-  return await Promise.all(
-    (
-      await globby(custuomPath ?? "websites/*/*/metadata.json")
->>>>>>> b5dbc10 (feat: vscode extension)
     ).map(async (s) => JSON.parse(await readFile(s, "utf-8")))
   );
 }
