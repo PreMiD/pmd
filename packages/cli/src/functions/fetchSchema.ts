d8a68008ef806aca978b963e28c0134ea6873edc
theusaf <theusafyt@gmail.com>
2024-03-24 14:14:34 -0700

    feat: use latest schema rather than hardcoding it

--- packages/cli/src/functions/fetchSchema.ts
+++ packages/cli/src/functions/fetchSchema.ts
@@ -1,5 +1,30 @@
 import fetch from "cross-fetch";
+import semver from "semver";
 
+const fallbackVersion = "1.10";
+
+export async function getLatestSchema(): Promise<string> {
+  const fetchedSchemas = await fetch(
+    "https://api.github.com/repos/PreMiD/Schemas/contents/schemas/metadata"
+  ).then((res) => res.json());
+
+  if (
+    fetchedSchemas.statusCode !== 200 ||
+    !Array.isArray(fetchedSchemas.body)
+  ) {
+    return fallbackVersion;
+  }
+  const schemas: string[] = fetchedSchemas.body.map(
+    (schema: { name: string }) => schema.name.replace(".json", "")
+  );
+  schemas.sort((a, b) => {
+    return semver.gt(a, b) ? -1 : 1;
+  });
+  return schemas[0];
+}
 export default async function fetchSchema() {
-	return (await fetch("https://schemas.premid.app/metadata/1.9")).json();
+  const latestSchema = await getLatestSchema();
+  return (
+    await fetch(`https://schemas.premid.app/metadata/1.${latestSchema}`)
+  ).json();
 }
